7d0f4e881b388fd2fd56ef654f80fda5eb70ef16
Jason Huang <j594huan@uwaterloo.ca>
2025-03-28 03:35:50 -0400

    can handler progress

---
+++ can_handler.c
@@ -0,0 +1,63 @@
+#include "can_handler.h"
+
+void can_setup(void) {
+    CANRXPPS = 0x11;    // CAN receive pin = RC1
+    RC0PPS = 0x33;      // CAN transmit pin = RC0
+
+    // init gpio pins
+    // tx
+    TRISC0 = 0; // out
+    LATC0 = 1;
+    ODCC0 = 1;
+    ANSELC0 = 0;
+
+    // rx
+    TRISC1 = 1; // in
+    ANSELC1 = 0;
+
+    can_timing_t timing;
+    can_generate_timing_params(_XTAL_FREQ, &timing);
+    can_init(&timing, can_receive_callback);
+
+}
+
+void can_receive_callback(const can_msg_t *msg) {
+    // ignore messages that were sent from this board (Primary and Failsafe)
+    if (get_board_type_unique_id(msg) == BOARD_TYPE_UNIQUE_ID) {
+        return;
+    }
+    uint16_t msg_type = get_message_type(msg);
+    int actuator_id;
+    int actuator_state;
+    
+    switch(msg_type) {
+#if (BOARD_INST_UNIQUE_ID == BOARD_INST_ID_CANARD_MOTOR_FAILSAFE)
+        case MSG_ACTUATOR_CMD:
+            actuator_id = get_actuator_id(msg);
+            if (actuator_id == ACTUATOR_CANARD_ENABLE) {
+                actuator_state = get_cmd_actuator_state(msg);
+                if (actuator_state == ACT_STATE_ON) {
+                    TRISC5 = 0;
+                }
+                else if (actuator_state == ACT_STATE_OFF) {
+                    TRISC5 = 1;
+                }
+            }
+            
+#elif (BOARD_INST_UNIQUE_ID == BOARD_INST_ID_CANARD_MOTOR_PRIMARY)
+        case MSG_ACTUATOR_CMD_ANALOG:
+            actuator_state = get_cmd_actuator_state_analog (msg);
+            if (actuator_state >= 0 && actuator_state <=200) {
+                updatePulseWidth(actuator_state);
+            }
+            
+            
+#endif
+            
+        default:
+            break;
+            
+            
+    }
+   
+}
--- setup.h
+++ setup.h
@@ -12,20 +12,6 @@
 
 #define HEARTBEAT() (LATA0 = !LATA0)
 
-// LEDs
-#if (BOARD_INST_UNIQUE_ID == BOARD_INST_ID_CANARD_MOTOR_PRIMARY)
-#define RED_LED_ON() (LATA0 = 0)
-#define RED_LED_OFF() (LATA0 = 1)
-#define GREEN_LED_ON() (LATA1 = 0)
-#define GREEN_LED_OFF() (LATA1 = 1)
-
-#elif (BOARD_INST_UNIQUE_ID == BOARD_INST_ID_CANARD_MOTOR_FAILSAFE)
-#define WHITE_LED_ON() (LATA0 = 0)
-#define WHITE_LED_OFF() (LATA1 = 1)
-#define BLUE_LED_ON() (LATA0 = 0)
-#define BLUE_LED_OFF() (LATA1 = 1)
-#endif
-
 void pin_init(void);
 
 void osc_init(void);
